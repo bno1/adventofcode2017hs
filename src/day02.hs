a1421716591c8864c3d9698e22e48fbd740df788
bno1 <ealex95@gmail.com>
2017-12-03 01:56:56 +0200

    day02 solved

---
+++ src/day02.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import Control.Arrow ((***))
+import Data.List (tails)
+
+
+minMax :: Integral a => [a] -> a
+minMax [] = 0
+minMax l = uncurry (-) $ foldr1 (\(x, y) -> max x *** min y) $ zip l l
+
+quotient :: Integral a => [a] -> a
+quotient [] = 0
+quotient l = head [
+        q
+        | (x:xs) <- tails l
+        , y <- xs
+        , let (q, r) = quotRem (max x y) (min x y)
+        , r == 0
+    ]
+
+main :: IO ()
+main = do
+    matrix <- (map (map read . words) . lines) <$> getContents
+
+    putStr "Solution 1: "
+    print $ sum $ map minMax matrix
+
+    putStr "Solution 2: "
+    print $ sum $ map quotient matrix
