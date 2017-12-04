73cebaeabe77a43facd13c8bbbfca61fe130cbe9
bno1 <ealex95@gmail.com>
2017-12-04 19:47:37 +0200

    fixed inconsistency

--- src/day04.hs
+++ src/day04.hs
@@ -5,15 +5,16 @@ import qualified Data.Set as S
 
 
 hasDuplicates :: (Traversable t, Ord a) => t a -> Bool
-hasDuplicates = and . snd . mapAccumL f S.empty
-    where f s x = let s' = S.insert x s in (s', S.size s' /= S.size s)
+hasDuplicates = or . snd . mapAccumL f S.empty
+    where f s x = let s' = S.insert x s in (s', S.size s' == S.size s)
 
 main :: IO ()
 main = do
     input <- lines <$> getContents
+    let count = length . filter id
 
     putStr "Solution 1: "
-    print $ length $ filter id $ map (hasDuplicates . words) input
+    print $ count $ map (not . hasDuplicates . words) input
 
     putStr "Solution 2: "
-    print $ length $ filter id $ map (hasDuplicates . map sort . words) input
+    print $ count $ map (not . hasDuplicates . map sort . words) input
